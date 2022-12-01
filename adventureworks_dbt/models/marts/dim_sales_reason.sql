8e25d287414e3fd4bdfbf4811091a4d8370eb94b
Vinicius Pereira <vinicius.pereira@indicium.tech>
2022-12-01 15:03:28 -0300

    Fixing errors after dbt run and test.

--- adventureworks_dbt/models/marts/dim_sales_reason.sql
+++ adventureworks_dbt/models/marts/dim_sales_reason.sql
@@ -10,17 +10,22 @@ with sales_reason as (
         select 
             sales_order_id
             , sales_reason_id
-        from  from {{ref('src_salesorderheadersalesreason')}}
+        from  {{ref('src_salesorderheadersalesreason')}}
     )
 
     , sales_reason_combined as (
         select
-            sales_reason_id
-            , sales_order_id
-            , reason_name
+            sales_order_header_reason.sales_order_id
+            , sales_reason.reason_type
         from sales_reason
         left join sales_order_header_reason on sales_order_header_reason.sales_reason_id = sales_reason.sales_reason_id
     )
 
+    , final_table as (
+        select *
+        from sales_reason_combined
+        where sales_order_id is not null
+    )
+
 select *
-from sales_reason_combined
+from final_table
