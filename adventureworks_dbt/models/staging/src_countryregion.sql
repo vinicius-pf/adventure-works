133394dee586f7841802b198f9dbf0cc3a5a482b
Vinicius Pereira <vinicius.pereira@indicium.tech>
2022-12-01 14:08:50 -0300

    Creating dim_geography.sql

--- adventureworks_dbt/models/staging/src_countryregion.sql
+++ adventureworks_dbt/models/staging/src_countryregion.sql
@@ -1,5 +1,5 @@
 select 
-    countryregioncode
-    , name
+    countryregioncode as country_id
+    , name as country_name
     , _sdc_received_at as ind_extraction_date
 from {{ source('adventure_works_etl', 'countryregion')}}
