a6dd912d937e8b09e16c31ed257f0cc775643d7f
Vinicius Pereira <vinicius.pereira@indicium.tech>
2022-12-05 09:44:09 -0300

    Creating and documenting dim_salesperson.

--- adventureworks_dbt/models/staging/src_employee.sql
+++ adventureworks_dbt/models/staging/src_employee.sql
@@ -1,10 +1,9 @@
 select 
-    businessentityid
+    businessentityid as businessentity_id
     , gender
-    , hiredate
-    , jobtitle
-    , currentflag
-    , modifieddate
-    , maritalstatus
+    , hiredate as hire_date
+    , jobtitle as job_title
+    , currentflag as current_flag
+    , modifieddate as modified_date
     , _sdc_received_at as ind_extraction_date
 from {{ source('adventure_works_etl', 'employee')}}
--- adventureworks_dbt/models/staging/src_salesperson.sql
+++ adventureworks_dbt/models/staging/src_salesperson.sql
@@ -1,9 +1,9 @@
 select
-    businessentityid 
-    , territoryid
-    , commissionpct
-    , saleslastyear
+    businessentityid as businessentity_id
+    , territoryid as territory_id
+    , commissionpct as commission_pct
+    , saleslastyear as sales_last_year
     , bonus
-    , salesquota
+    , salesquota as sales_quota
     , _sdc_received_at as ind_extraction_date
 from {{ source('adventure_works_etl', 'salesperson')}}
