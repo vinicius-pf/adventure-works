e153371b6c7ee4fa85fd5fd4fbca16b4e8de7cf7
Vinicius Pereira <vinicius.pereira@indicium.tech>
2022-12-07 13:46:17 -0300

    Creating reason_sk and documenting better.

--- adventureworks_dbt/models/marts/dim_sales_reason.sql
+++ adventureworks_dbt/models/marts/dim_sales_reason.sql
@@ -16,15 +16,20 @@ with sales_reason as (
     , sales_reason_combined as (
         select
             sales_order_header_reason.sales_order_id
+            , sales_order_header_reason.sales_reason_id
             , sales_reason.reason_type
         from sales_reason
         left join sales_order_header_reason on sales_order_header_reason.sales_reason_id = sales_reason.sales_reason_id
     )
 
     , final_table as (
-        select *
+        select 
+            {{ dbt_utils.surrogate_key(['sales_order_id', 'sales_reason_id']) }} as reason_sk
+            , sales_order_id
+            , sales_reason_id
+            , reason_type
         from sales_reason_combined
-        where sales_order_id is not null
+        where sales_order_id is not null --There are only 3 registers without a sales_order_id.
     )
 
 select *
